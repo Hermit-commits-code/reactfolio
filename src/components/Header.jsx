025b66198162aae8e2a4abb11249dae8873a3e57
Joseph Chu <hotcupofjoe2013@gmail.com>
2024-11-25 18:21:46 -0500

    Added Navbar, closing and menu icons

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./styles/Header.scss";
+import { Close, MenuBookOutlined } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <h1>Reactfolio</h1>
+      </div>
+      <nav>
+        <ul>
+          <div className="closed">
+            <Close className="close" />
+          </div>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="change">
+        <MenuBookOutlined className="menu" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
